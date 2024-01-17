576f55e9d6b0637d2ea9376ab6b7f675b217395a
Rahul Tripathi <rahul@clouddefense.io>
2024-01-17 17:50:26 +0530

    enhancements
    Signed-off-by: Rahul Tripathi <rahul@clouddefense.io>

--- libs/community/langchain_community/document_loaders/daxa.py
+++ libs/community/langchain_community/document_loaders/daxa.py
@@ -8,7 +8,7 @@ from http import HTTPStatus
 
 from langchain_community.utilities.daxa import CLASSIFIER_URL, PLUGIN_VERSION
 from langchain_community.utilities.daxa import get_loader_full_path, get_loader_type, get_full_path, get_runtime
-from langchain_community.utilities.daxa import App
+from langchain_community.utilities.daxa import App, Doc
 from langchain_community.document_loaders.base import BaseLoader
 
 logger = logging.getLogger(__name__)
@@ -18,9 +18,11 @@ class DaxaSafeLoader(BaseLoader):
 
     def __init__(self, langchain_loader: BaseLoader, app_id: str, owner: str):
         if not app_id or not isinstance(app_id, str):
-            raise NameError("""No app_id provided. Or invalid app_id.""")
+            raise NameError("""No app_id is passed or invalid app_id.""")
+        if not owner or not isinstance(owner, str):
+            raise NameError("""No owner is passed or invalid owner.""")
         self.app_name = app_id
-        self.loader_id = str(uuid.uuid4())
+        self.load_id = str(uuid.uuid4())
         self.loader = langchain_loader
         self.owner = owner
         self.source_path = get_loader_full_path(self.loader)
@@ -29,13 +31,12 @@ class DaxaSafeLoader(BaseLoader):
         source_type = get_loader_type(loader_name)
         self.loader_details = {
             "loader": loader_name,
-            "loader_id": self.loader_id,
             "source_path": self.source_path,
             "source_type": source_type
         }
         #generate app
-        app = self._get_app_details()
-        self._send_discover(app)
+        self.app = self._get_app_details()
+        self._send_discover()
 
     def load(self):
         """load Documents."""
@@ -80,19 +81,21 @@ class DaxaSafeLoader(BaseLoader):
             "owner": self.owner,
             "docs": [{"doc": doc.get('page_content'), "source_path": get_full_path(doc.get('metadata', {}).get('source')), "last_modified": doc.get('metadata', {}).get('last_modified')} for doc in doc_content],
             "plugin_version": PLUGIN_VERSION,
+            "load_id": self.load_id,
             "loader_details": self.loader_details,
             "loading_end": "false"
         }
         if loading_end is True:
             payload["loading_end"] = "true"
-        resp = requests.post(f"{CLASSIFIER_URL}/loader/doc", headers=headers, json=payload, timeout=10)
+        payload = Doc.model_validate(payload).model_dump(exclude_unset=True)
+        resp = requests.post(f"{CLASSIFIER_URL}/loader/doc", headers=headers, json=payload, timeout=20)
         logger.debug(f"===> send_loader_doc: request, url {resp.request.url}, headers {resp.request.headers}, body {resp.request.body[:999]} with a len: {len(resp.request.body)}\n")
         logger.debug(f"===> send_loader_doc: response status {resp.status_code}, body {resp.json()}\n")
 
-    def _send_discover(self, app: App):
+    def _send_discover(self):
         headers =  {'Accept': 'application/json', 'Content-Type': 'application/json'}
-        payload = app.model_dump(exclude_unset=True)
-        resp = requests.post(f"{CLASSIFIER_URL}/app/discover", headers=headers, json=payload)
+        payload = self.app.model_dump(exclude_unset=True)
+        resp = requests.post(f"{CLASSIFIER_URL}/app/discover", headers=headers, json=payload, timeout=20)
         logger.debug(f"===> send_discover: request, url {resp.request.url}, headers {resp.request.headers}, body {resp.request.body}\n")
         logger.debug(f"===> send_discover: response status {resp.status_code}, body {resp.json()}\n")
         if resp.status_code == HTTPStatus.OK or resp.status_code == HTTPStatus.BAD_GATEWAY:
@@ -103,7 +106,7 @@ class DaxaSafeLoader(BaseLoader):
         app = App(
             name=self.app_name,
             owner=self.owner,
-            loader_id=self.loader_id,
+            load_id=self.load_id,
             runtime=runtime,
             framework=framework,
             plugin_version=PLUGIN_VERSION,
